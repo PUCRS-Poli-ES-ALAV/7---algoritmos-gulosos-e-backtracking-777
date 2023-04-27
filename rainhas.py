33712e6c20c35dc0eb1f15a9a0555c5f076a9c08
Daniele Paz <42788455+dragonqveen@users.noreply.github.com>
2023-04-27 21:34:51 +0000

    :bug: Fix reference bug

--- rainhas.py
+++ rainhas.py
@@ -1,26 +1,32 @@
 def calcula_posicoes_atq(pos_rainha):
-    posicoes_atq = [[0]*7]*7
+    posicoes_atq = [
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0]]
 
-    posicoes_atq[3] = [1]*7
+    posicoes_atq[3] = [1, 1, 1, 1, 1, 1, 1]
+
     for linha in posicoes_atq:
         linha[3] = 1
 
     n = 0
-
-    #print(posicoes_atq)
     for linha in posicoes_atq:
         linha[n] = 1
-        print(linha)
         n = n + 1
-    
-    #posicoes_atq[3][3] = 2
+        linha[7-n] = 1
 
-    return posicoes_atq
+    posicoes_atq[pos_rainha[0]][pos_rainha[1]] = 2
+    printa_tabuleiro(posicoes_atq)
 
 def printa_tabuleiro(tabuleiro):
+    print()
     for linha in tabuleiro:
         print(linha)
 
-#printa_tabuleiro(calcula_posicoes_atq([4,4]))
+#printa_tabuleiro(calcula_posicoes_atq())
 
-calcula_posicoes_atq([4,4])
+calcula_posicoes_atq([3,3])
